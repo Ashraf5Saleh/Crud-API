f2a276a00cc01ae713cdfeed1b3882dc52a3483c
Ashraf5Saleh <ashrafsaleh8602@gmail.com>
2025-10-11 18:17:34 +0300

    Initial commit of existing project

---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import express, { json } from 'express'
+const app = express()
+const port = 3000
+
+
+
+var products = [
+  { id: 1, name: 'shoes', price: 50, category: 'footwear' },
+  { id: 2, name: 't-shirt', price: 80, category: 'clothes' },
+  { id: 3, name: 'jeans', price: 75, category: 'clothes' },
+  { id: 4, name: 'neckalce', price: 200, category: 'accessories' },
+]
+
+app.use(express.json())
+// fetch products
+app.get('/app/products', (req, res) => {
+  const requestedcategory = req.query.category
+  if (requestedcategory) {
+    res.json(products.filter(products => products.category === requestedcategory))
+  } else {
+    res.json(products)
+  }
+})
+
+// create products 
+app.post('/api/products', (req, res) => {
+  console.log(req.body)
+   var lastid = products.length
+
+   const productWithId = { id: lastid + 1, ...req.body };
+
+  products.push(productWithId);
+
+  res.json({
+    message: 'Product has been created',
+    product: productWithId,
+  });
+
+})
+
+// Update products
+
+app.patch('/api/products/:name', (req, res) => {
+  const name = req.params.name
+  const data = req.body
+  products = products.map(product => {
+    if (product.name === name) {
+      const newproduct = {
+        id: data.id || product.id,
+        name: data.name || product.name,
+        price: data.price || product.price,
+        category: data.category || product.category
+      }
+      return newproduct;
+    } else {
+      return product;
+    }
+  })
+  res.json({ Message: `product ${name} has been updated` })
+})
+
+// delete products 
+
+app.delete('/api/products/:name', (req, res) => {
+  console.log(req.params)
+  const name = req.params.name
+  products = products.filter(product => product.name !== name)
+  res.json({ message: `product ${name} has been deleted` })
+})
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`)
+})
